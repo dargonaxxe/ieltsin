2647c8b3afa35a28dd26d29704f350976b8334d6
Ilia Kuzmin <dargonaxxe@gmail.com>
2024-01-09 17:27:40 +0400

    added telegex dependency

---
+++ lib/application.ex
@@ -0,0 +1,9 @@
+defmodule Ieltsin.Application do
+  use Application
+
+  def start(_, _) do
+    children = []
+    opts = [strategy: :one_for_one, name: Ieltsin.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
