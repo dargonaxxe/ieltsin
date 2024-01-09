ea39cbdecae98bdc20f7b04a652229c7acff5520
Ilia Kuzmin <dargonaxxe@gmail.com>
2024-01-09 17:25:29 +0400

    initial commit

---
+++ lib/ieltsin.ex
@@ -0,0 +1,18 @@
+defmodule Ieltsin do
+  @moduledoc """
+  Documentation for `Ieltsin`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ieltsin.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ieltsin_test.exs
@@ -0,0 +1,8 @@
+defmodule IeltsinTest do
+  use ExUnit.Case
+  doctest Ieltsin
+
+  test "greets the world" do
+    assert Ieltsin.hello() == :world
+  end
+end
